5ce19bdfe4ac41d29922866be04e92c63aef62ea
aaksac <53212521+aaksac@users.noreply.github.com>
2026-04-24 14:51:32 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -648,7 +648,7 @@ function focusMapForPickedLocation(lat, lng) {
     return;
   }
 
-  const targetZoom = 17;
+  const targetZoom = 20;
   const currentZoom = Number(map.getZoom()) || 0;
   const shouldZoomIn = currentZoom < targetZoom;
 
