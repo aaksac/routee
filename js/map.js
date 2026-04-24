90d876b7173cef1fa9564ee04602a1e621b3e7ac
aaksac <53212521+aaksac@users.noreply.github.com>
2026-04-24 23:16:57 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -41,7 +41,7 @@ const POINT_COLORS = [
 const MAP_CLICK_PROGRESSIVE_ZOOM_LEVELS = [
   { maxZoom: 4, targetZoom: 6 },
   { maxZoom: 9, targetZoom: 11 },
-  { maxZoom: 13, targetZoom: 15 },
+  { maxZoom: 14, targetZoom: 16 },
   { maxZoom: 19, targetZoom: 20 }
 ];
 
