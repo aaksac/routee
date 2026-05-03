220be22f333ccaab5f8ea8fe601c3de626137bb1
aaksac <53212521+aaksac@users.noreply.github.com>
2026-05-04 01:17:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const SW_VERSION = new URL(self.location.href).searchParams.get("v") || "dev";
 const CACHE_NAME = `routee-shell-${SW_VERSION}`;
 const NETWORK_TIMEOUT_MS = 1200;
-const ROUTEE_ASSET_REVISION = "20260503-tablet-splash-ratio";
+const ROUTEE_ASSET_REVISION = "20260504-mobile-topbar-final";
 
 const APP_SHELL = [
   "./",
