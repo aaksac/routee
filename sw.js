6f33174c4efcebb6bb1e9d5ffc43ad389b729d34
aaksac <53212521+aaksac@users.noreply.github.com>
2026-05-04 03:04:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const SW_VERSION = new URL(self.location.href).searchParams.get("v") || "dev";
 const CACHE_NAME = `routee-shell-${SW_VERSION}`;
 const NETWORK_TIMEOUT_MS = 1200;
-const ROUTEE_ASSET_REVISION = "20260504-tablet-trip-mobile-card-flow";
+const ROUTEE_ASSET_REVISION = "20260504-tablet-portrait-final-one-card-flow";
 
 const APP_SHELL = [
   "./",
