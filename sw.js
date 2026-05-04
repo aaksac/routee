a43b9d6d53477e713fce9f31bed4e5e5155ea816
aaksac <53212521+aaksac@users.noreply.github.com>
2026-05-04 03:37:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const SW_VERSION = new URL(self.location.href).searchParams.get("v") || "dev";
 const CACHE_NAME = `routee-shell-${SW_VERSION}`;
 const NETWORK_TIMEOUT_MS = 1200;
-const ROUTEE_ASSET_REVISION = "20260504-tablet-actions-align-content-start";
+const ROUTEE_ASSET_REVISION = "20260504-tablet-top-controls-aligned";
 
 const APP_SHELL = [
   "./",
